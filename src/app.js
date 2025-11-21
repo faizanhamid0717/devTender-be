fa7e8a6c1f619d8184caf392b685dfa8f302fe20
faizanhamid0717 <faizanhamid162@gmail.com>
2025-11-21 19:26:10 +0530

    commited get post routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express()   // create an express application or server
 
-app.use('/test',(req,res)=>{
-   res.end('Hello World by nodemon');
+
+app.use("/hello",(req,res)=>{
+    res.send('Hello from /hello/2 endpoint');
 })
 
+app.get("/user",(req,res)=>{
+    res.send({name:'Faizan',age:22});
+})
+app.post("/user",(req,res)=>{
+    res.send('Post request to /user endpoint received');
+})
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
