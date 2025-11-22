cd75c9beea572ce8c75ab780fd338dcbe40e9952
faizanhamid0717 <faizanhamid162@gmail.com>
2025-11-22 11:21:11 +0530

    playing-routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express()   // create an express application or server
 
-
-app.use("/hello",(req,res)=>{
-    res.send('Hello from /hello/2 endpoint');
+app.use("/user",[(req,res,next)=>{
+    // res.send("Welcome to DevTinder Backend")
+    next()
+},(req,res,next)=>{
+    // res.send("This is second callback")
+    next()
+}],(req,res,next)=>{
+    res.send("This is 3rd callback")
+    next()
+},(req,res,next)=>{
+    res.send("This is 4th callback")
+    // next()
 })
 
-app.get("/user",(req,res)=>{
-    res.send({name:'Faizan',age:22});
-})
-app.post("/user",(req,res)=>{
-    res.send('Post request to /user endpoint received');
-})
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
