3ca5171fcf88b7785ce2d716ef27d98f60145576
faizanhamid0717 <faizanhamid162@gmail.com>
2025-11-23 14:52:50 +0530

    connect db and added signup route and user in db

--- src/app.js
+++ src/app.js
@@ -1,23 +1,32 @@
 const express = require("express");
+const { connectToDatabase } = require("./config/database");
+const UserModel = require("./models/userSchema");
 const app = express(); // create an express application or server
 
-
-app.get("/getUserData", (req, res) => {
-    try {
-        res.send("user data!");
-    } catch (error) {
-        res.status(500).send("Internal Server Error");
-    }
-  
-});
-
-// use at end of all routes
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    return res.status(500).send("Internal Server Error");
+app.post("/signup", async (req, res) => {
+  const user = new UserModel({
+    firstName: "Faheem",
+    lastName: "Hamid",
+    emailId: "faheemw09@gmail.com",
+    password: "faheem123",
+    age: 27,
+    gender: "male",
+  });
+  try {
+    await user.save();
+    res.status(201).send("User signed up successfully");
+  } catch (error) {
+    res.status(400).send("Error signing up user :" + error.message);
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+connectToDatabase()
+  .then(() => {
+    console.log("Connected to the database successfully");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed:", error);
+  });
