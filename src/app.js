aea96e81a7364c8f7d3813cdea734ca17a27ff7a
faizanhamid0717 <faizanhamid162@gmail.com>
2025-11-29 12:47:25 +0530

    tender-CRUD-user

--- src/app.js
+++ src/app.js
@@ -17,6 +17,82 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(404).send("Error fetching users :" + error.message);
+  }
+});
+
+// get one user
+app.get("/user", async (req, res) => {
+  const userAge = req.body.age;
+  try {
+    const User = await UserModel.findOne({ age: userAge });
+    if (!User) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(User);
+    }
+  } catch (error) {
+    res.status(404).send("Error fetching user :" + error.message);
+  }
+});
+
+// get user by id
+app.get("/userbyid", async (req, res) => {
+  const ID = req.body.id;
+  try {
+    const UserId = await UserModel.findById({ _id: ID });
+    if (!UserId) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(UserId);
+    }
+  } catch (error) {
+    res.status(404).send("Error fetching user by ID :" + error.message);
+  }
+});
+
+// update user by id
+app.patch("/updateuser", async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  try {
+    const updateUser = await UserModel.findByIdAndUpdate(
+      data.id,  
+      data
+      //   {
+      //     firstName: data.firstName,
+      //     emailId: data.emailId,
+      //     age: data.age,
+      //     gender: data.gender,
+      //   },
+    );
+    res.send("User updated successfully: " + updateUser);
+  } catch (error) {
+    res.status(404).send("Error updating user :" + error.message);
+  }
+});
+
+// deleyte user by id
+app.delete("/deleteuser", async (req, res) => {
+  const ID = req.body.id;
+  try {
+    const data = await UserModel.findByIdAndDelete(ID);
+    if (!data) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("User deleted successfully");
+    }
+  } catch (error) {
+    res.status(404).send("Error deleting user :" + error.message);
+  }
+});
+// Start the server after connecting to the database
 connectToDatabase()
   .then(() => {
     console.log("Connected to the database successfully");
